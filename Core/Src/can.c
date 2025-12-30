248f19b35e8f31b201f6543901532cd64e113c5a
OmerGunbakmaz <ognbkmz@gmail.com>
2025-12-30 22:31:59 +0300

    loopback mode off

--- Core/Src/can.c
+++ Core/Src/can.c
@@ -74,7 +74,7 @@ void MX_CAN1_Init(void)
   hcan1.State = HAL_CAN_STATE_RESET;
   hcan1.Instance = CAN1;
   hcan1.Init.Prescaler = 6;  // 3 → 6 oldu
-  hcan1.Init.Mode =  CAN_MODE_LOOPBACK;;
+  hcan1.Init.Mode =  CAN_MODE_NORMAL;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan1.Init.TimeSeg1 = CAN_BS1_11TQ;
   hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
